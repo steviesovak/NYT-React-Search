2e2067b2deac8add34b3878e2fe3513da6e10325
Steven Sovak <sjs0509@gmail.com>
2018-10-31 14:34:52 -0500

    controller/comp

---
+++ controllers/articleController.js
@@ -0,0 +1,46 @@
+const db = require("../models");
+
+module.exports = {
+    //find articles
+  findAll: function(req, res) {
+    db.Article
+      .find(req.query)
+      .sort({ date: -1 })
+      .then(dbArticle => res.json(dbArticle))
+      .catch(err => res.status(422).json(err));
+  },
+  // find by id
+  findById: function(req, res) {
+    db.Article
+      .findById(req.params.id)
+      .then(dbArticle => res.json(dbArticle))
+      .catch(err => res.status(422).json(err));
+  },
+  //create 
+  create: function(req, res) {
+    const article = {
+      _id: req.body._id,
+      title: req.body.headline.main,
+      url: req.body.web_url
+    };
+    db.Article
+      .create(article)
+      .then(dbArticle => res.json(dbArticle))
+      .catch(err => res.status(422).json(err));
+  },
+  //update
+  update: function(req, res) {
+    db.Article
+      .findOneAndUpdate({ _id: req.params.id }, req.body)
+      .then(dbArticle => res.json(dbArticle))
+      .catch(err => res.status(422).json(err));
+  },
+  //delete
+  remove: function(req, res) {
+    db.Article
+      .findById({ _id: req.params.id })
+      .then(dbArticle => dbArticle.remove())
+      .then(dbArticle => res.json(dbArticle))
+      .catch(err => res.status(422).json(err));
+  }
+};
